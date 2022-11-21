be29984f5332f9760b69bd97c297a92b66d6706c
RONIT AFTER <ronitafter@gmail.com>
2022-11-21 12:37:28 +0200

    nav

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -23,12 +23,44 @@ const NavButton = ({ title, customFunc, icon, color, dotColor }) => (
 )
 
 const Navbar = () => {
-	const { activeMenu, setActiveMenu } = useStateContext();
+	const { activeMenu, setActiveMenu, isClicked, setIsClicked, handleClick } = useStateContext();
 	return (
 		<div className='flex justify-between p-2 md:mx-6 relative'>
 			<NavButton title="Menu" customFunc={() =>
 				setActiveMenu((prevActiveMenue) => !prevActiveMenue)}
 				color="blue" icon={<AiOutlineMenu />} />
+			<div className='flex'>
+				<NavButton title="Cart"
+					customFunc={() => handleClick('cart')}
+					color="blue" icon={<FiShoppingCart />} />
+				<NavButton
+					title="Chat"
+					dotColor="#03C9D7"
+					customFunc={() => handleClick('chat')}
+					color="blue" icon={<BsChatLeft />} />
+				<NavButton
+					title="Notifications"
+					dotColor="#03C9D7"
+					customFunc={() => handleClick('notification')}
+					color="blue" icon={<RiNotification3Line />} />
+				<TooltipComponent content="Profile" position='BottomCenter'>
+					<div className='flex items-center gap-2 cursor-pointer p-1 hover:bg-light-gray rounded-lg'
+						onClick={() => handleClick('userProfile')}
+					>
+						<img className='rounded-full w-8 h-8' src={avatar} />
+						<p>
+							<span className='text-gray-400 text-14'>Hi, </span> {' '}
+							<span className='text-gray-400 font-bold ml-1 text-14'>Michael</span>
+						</p>
+						<MdKeyboardArrowDown className='text-gray-400 text-14' />
+					</div>
+				</TooltipComponent>
+				{isClicked.cart && <Cart />}
+				{isClicked.chat && <Chat />}
+				{isClicked.notification && <Notification />}
+				{isClicked.userProfile && <UserProfile />}
+				{isClicked.cart && <Cart />}
+			</div>
 		</div>
 	)
 }
