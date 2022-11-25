109be138081f849c9085a72c3d7f9fb86f2fdd65
ronitafter <54575125+ronitafter@users.noreply.github.com>
2022-11-25 18:01:55 +0200

    menu2

--- src/pages/Ecommerce.jsx
+++ src/pages/Ecommerce.jsx
@@ -28,20 +28,20 @@ const Ecommerce = () => {
 				<div className='flex m-3 flex-wrap justify-center gap-1 items-center'>
 					{earningData.map((item) => (
 						<div key={item.title} className="
-						bg-white 
-						dark:text-gray-200
-						dark:bg=secondary-dark-bg
-						md:w-56
-						p-4
-						pt-9
-						rounded-2xl">
+						bg-neutral-50 h-44 dark:text-gray-200 dark:bg-secondary-dark-bg md:w-46  p-4 pt-9 rounded-2xl">
 							<button
 								type='button'
 								style={{ color: item.iconColor, backgroundColor: item.iconBg }}
 								className="text-2xl opacity-0.9 rounded-full p-4 hover:drop-shadow-xl"
 							>{item.icon}
 							</button>
-							<p className='mt-3'><span className='text-lg font-smibold'>{item.amount}</span></p>
+							<p className='mt-3'>
+								<span className="text-lg font-semibold">{item.amount}</span>
+								<span className={`text-sm text-${item.pcColor} ml-2`}>
+									{item.percentage}
+								</span>
+							</p>
+							<p>{item.title}</p>
 						</div>
 					))}
 				</div>
