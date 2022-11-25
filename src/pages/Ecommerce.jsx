7e345530384b6bb0c57196fdc0b4ec4aba8754ab
ronitafter <54575125+ronitafter@users.noreply.github.com>
2022-11-25 18:53:32 +0200

    menu3

--- src/pages/Ecommerce.jsx
+++ src/pages/Ecommerce.jsx
@@ -9,7 +9,7 @@ const Ecommerce = () => {
 	return (
 		<div className="mt-24">
 			<div className="flex flex-wrap lg:flex-nowrap justify-center ">
-				<div className="bg-white dark:text-gray-200 dark:bg-secondary-dark-bg h-44 rounded-xl w-full lg:w-85 p-8 pt-9 m-3 bg-hero-pattern bg-no-repeat bg-cover bg-center">
+				<div className="bg-white dark:text-gray-200 dark:bg-secondary-dark-bg h-44 rounded-xl w-full lg:w-80 p-8 pt-9 m-3 bg-hero-pattern bg-no-repeat bg-cover bg-center">
 					<div className="flex justify-between items-center">
 						<div>
 							<p className="font-bold text-gray-400">Earnings</p>
@@ -27,8 +27,7 @@ const Ecommerce = () => {
 				</div>
 				<div className='flex m-3 flex-wrap justify-center gap-1 items-center'>
 					{earningData.map((item) => (
-						<div key={item.title} className="
-						bg-neutral-50 h-44 dark:text-gray-200 dark:bg-secondary-dark-bg md:w-46  p-4 pt-9 rounded-2xl">
+						<div key={item.title} className="bg-white h-44 dark:text-gray-200 dark:bg-secondary-dark-bg md:w-56  p-4 pt-9 rounded-2xl">
 							<button
 								type='button'
 								style={{ color: item.iconColor, backgroundColor: item.iconBg }}
@@ -41,9 +40,38 @@ const Ecommerce = () => {
 									{item.percentage}
 								</span>
 							</p>
-							<p>{item.title}</p>
+							<p className='text-sm text-gray-400 mt-1'>{item.title}</p>
 						</div>
 					))}
+				</div>
+			</div>
+			<div className='flex gap-10 flex-wrap justify-center'>
+				<div className='bg-white dark:text-gray-200 dark:bg-secondary-dark-bg m-3 p-4 rounded-2xl md:w-780'>
+					<div className='flex justify-between'>
+						<p className="font-semibold text-xl">Revenue Updates</p>
+						<div className='flex item-center gap-4'>
+							<p className='flex items-center gap-2 text-gray-600
+							hover:drop-shadow-xl'>
+								<span><GoPrimitiveDot /></span>
+								<span>Expense</span>
+							</p>
+							<p className='flex items-center gap-2 text-green-400
+							hover:drop-shadow-xl'>
+								<span><GoPrimitiveDot /></span>
+								<span>Budget</span>
+							</p>
+						</div>
+					</div>
+					<div className='flex-wrap justify-center'>
+						<div className='border-r-1 border-color m-4 pr-10'>
+							<p>
+								<span className='text-3xl font-semibold'>$93,438</span>
+								<span className='p-1.5 hover:drop-shadow-xl cursor-pointer rounded-full text-white bg-green-400 ml-3 text-xs'>
+									23%
+								</span>
+							</p>
+						</div>
+					</div>
 				</div>
 			</div>
 		</div >
