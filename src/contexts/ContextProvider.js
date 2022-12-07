75d262a224efe202e539cca5fa2e84ec1f107f8e
RONIT AFTER <ronitafter@gmail.com>
2022-12-07 12:46:40 +0200

    gfgf

--- src/contexts/ContextProvider.js
+++ src/contexts/ContextProvider.js
@@ -1,54 +1,55 @@
-import React, { useState, useContext, createContext } from "react";
+import React, { createContext, useContext, useState } from "react";
 
 const StateContext = createContext();
+
 const initialState = {
   chat: false,
-  chart: false,
-  userOrofile: false,
+  cart: false,
+  userProfile: false,
   notification: false,
 };
 
 export const ContextProvider = ({ children }) => {
-  const [activeMenu, setActiveMenu] = useState(true);
-  const [isClicked, setIsClicked] = useState(initialState);
   const [screenSize, setScreenSize] = useState(undefined);
   const [currentColor, setCurrentColor] = useState("#03C9D7");
   const [currentMode, setCurrentMode] = useState("Light");
-  const [themeSettimgs, setThemeSettings] = useState(false);
+  const [themeSettings, setThemeSettings] = useState(false);
+  const [activeMenu, setActiveMenu] = useState(true);
+  const [isClicked, setIsClicked] = useState(initialState);
 
   const setMode = (e) => {
     setCurrentMode(e.target.value);
     localStorage.setItem("themeMode", e.target.value);
-    setThemeSettings(false);
   };
 
   const setColor = (color) => {
     setCurrentColor(color);
     localStorage.setItem("colorMode", color);
-    setThemeSettings(false);
   };
 
-  const handleClick = (clicked) => {
+  const handleClick = (clicked) =>
     setIsClicked({ ...initialState, [clicked]: true });
-  };
+
   return (
+    // eslint-disable-next-line react/jsx-no-constructed-context-values
     <StateContext.Provider
       value={{
-        activeMenu,
-        setActiveMenu,
-        isClicked,
-        setIsClicked,
-        handleClick,
-        screenSize,
-        setScreenSize,
         currentColor,
         currentMode,
-        // setCurrentColor,
-        // setCurrentMode,
-        themeSettimgs,
-        setThemeSettings,
+        activeMenu,
+        screenSize,
+        setScreenSize,
+        handleClick,
+        isClicked,
+        initialState,
+        setIsClicked,
+        setActiveMenu,
+        setCurrentColor,
+        setCurrentMode,
         setMode,
         setColor,
+        themeSettings,
+        setThemeSettings,
       }}
     >
       {children}
